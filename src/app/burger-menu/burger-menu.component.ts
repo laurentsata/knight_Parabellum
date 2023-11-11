d6b4ee1e4fad58d9e0d7727d721d6f53ea9c690d
sata <laurentwardiab@gmail.com>
2023-11-11 22:03:52 +0100

    add tuto, effet menu burger, acces prive

--- src/app/burger-menu/burger-menu.component.ts
+++ src/app/burger-menu/burger-menu.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-burger-menu',
   templateUrl: './burger-menu.component.html',
   styleUrls: ['./burger-menu.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('in', style({ opacity: 1 })),
+      transition(':enter', [style({ opacity: 0 }), animate(80000)]),
+      transition(':leave', animate(1000, style({ opacity: 0 }))),
+    ]),
+  ],
 })
+
 export class BurgerMenuComponent {
   isMenuOpen = false;
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
   }
 }
