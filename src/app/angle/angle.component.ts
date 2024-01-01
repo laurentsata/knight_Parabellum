41ec653d4ba5c367fc9c7654580a6c7055395219
sata <laurentwardiab@gmail.com>
2024-01-01 17:24:58 +0100

    add converter mrad moa

---
+++ src/app/angle/angle.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-angle',
+  templateUrl: './angle.component.html',
+  styleUrls: ['./angle.component.scss']
+})
+export class AngleComponent {
+
+  mradValue!: number;
+  moaValue!: number;
+
+  convertToMOA() {
+    // Conversion de milliradians en minutes d'angle
+    this.moaValue = this.mradValue * 3.4377468;
+  }
+
+  convertToMRAD() {
+    // Conversion de minutes d'angle en milliradians
+    this.mradValue = this.moaValue /3.4377468;
+  }
+
+  resetValues() {
+    // Réinitialiser les valeurs à zéro
+    this.mradValue = 0;
+    this.moaValue = 0;
+  }
+}
