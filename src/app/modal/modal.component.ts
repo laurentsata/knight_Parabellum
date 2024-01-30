979cb55302db8dcaa6741aa07af03d7f061f350c
sata <laurentwardiab@gmail.com>
2024-01-30 09:00:36 +0100

    matdialogref

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 // import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -7,15 +8,15 @@ import { Component, Inject } from '@angular/core';
   styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent {
-  // constructor(
-  //   public dialogRef: MatDialogRef<ModalComponent>,
-  //   @Inject(MAT_DIALOG_DATA) public data: { imageUrl: string }
-  // ) { }
+  constructor(
+    public dialogRef: MatDialogRef<ModalComponent>,
+    // @Inject(MAT_DIALOG_DATA) public data: {  }
+  ) { }
 
   // // Fonction appelée lorsqu'on clique sur le bouton "Fermer"
-  // onNoClick(): void {
-  //   this.dialogRef.close();
-  // }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
 
 
