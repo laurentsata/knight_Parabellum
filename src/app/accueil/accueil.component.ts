e5602a5df269a6e2833d8fc4ea217730fb346b17
sata <95581517+laurentsata@users.noreply.github.com>
2024-03-10 17:17:31 +0100

    partenaire ta defense

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import { MatDialog } from '@angular/material/dialog';
-// import { ModalComponent } from '../modal/modal.component';
+ import { MatDialog } from '@angular/material/dialog';
+ import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-accueil',
@@ -9,21 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccueilComponent implements OnInit {
 
-  // constructor(public dialog: MatDialog) { }
+   constructor(public dialog: MatDialog) { }
 
    ngOnInit(): void {
   //   // Appel automatique de la fonction pour ouvrir la modal
-  //   this.openModal('');
+     this.openModal('');
   }
 
-  // openModal(imageUrl: string): void {
-  //   const dialogRef = this.dialog.open(ModalComponent, {
+   openModal(imageUrl: string): void {
+    const dialogRef = this.dialog.open(ModalComponent, {
 
   //     // data: { imageUrl: imageUrl }
-  //   });
+     });
 
-    // dialogRef.afterClosed().subscribe(result => {
-    //   console.log('The dialog was closed');
-    // });
-  // }
+     dialogRef.afterClosed().subscribe(result => {
+       console.log('The dialog was closed');
+     });
+   }
  }
