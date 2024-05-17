e3bca57068cbcf8b2299654ddb7aa1743a2cb9b4
sata <95581517+laurentsata@users.noreply.github.com>
2024-05-17 17:00:02 +0200

    nav section

--- src/app/tuto/tuto.component.ts
+++ src/app/tuto/tuto.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FragmentService } from '../fragment.service';
 
 @Component({
   selector: 'app-tuto',
   templateUrl: './tuto.component.html',
   styleUrls: ['./tuto.component.scss']
 })
-export class TutoComponent {
+
+export class TutoComponent implements OnInit {
+  isMenuOpen: boolean = false;
+  
+  constructor(private fragmentService: FragmentService) { }
+
+  ngOnInit(): void {
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+  navigateToFragment(fragment: string): void {
+    this.fragmentService.navigateToFragment(fragment);
+  }
 
 }
