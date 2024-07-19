afb8360a7e7c6387021b128fe441b0797fd0bc18
sata <95581517+laurentsata@users.noreply.github.com>
2024-07-19 14:40:41 +0200

    angular 18.1.1

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-converter',
   templateUrl: './converter.component.html',
-  styleUrls: ['./converter.component.scss']
+  styleUrls: ['./converter.component.scss'],
 })
 export class ConverterComponent {
   grains!: number;
